5927a7125b6713ec653c9a3b565937d54e057330
Jacob Kostecki <jacob@kostecki.dk>
2025-06-26 21:57:14 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+const config = {
+  eventId: "555893",
+  eventUrl:
+    "https://www.ticketmaster.dk/event/roskilde-festival-2025-one-day-ticket-friday-billetter/555893",
+  eventName: "Roskilde Festival 2025 - One-Day Ticket, Friday",
+};
+
+const ntfyTopic = "nt-rf";
+
+const checkForTickets = async () => {
+  console.log("Fetching data for event:", config.eventName + "\n");
+
+  const url = `https://availability.ticketmaster.dk/api/v2/TM_DK/resale/${config.eventId}`;
+
+  const headers = new Headers({
+    Cookie:
+      "eps_sid=b7527c16a8c40017eb7a59486c71ec364041496d;tmpt=0:c1bd357bbe000000:1750855624:ab5b3824:01691fe117d0816f44f95a25c823ea41:f76ec1290039b5788f9af3514dd8e1eff64adfc3485fb3e0d08a6ecf9bdf33f6",
+  });
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: headers,
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+const sendSuccessNotification = () => {
+  console.log("Sending notification...");
+
+  fetch("https://ntfy.israndom.win", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      topic: ntfyTopic,
+      title: "DER ER BILLETTER?!",
+      message: config.eventName,
+      actions: [
+        {
+          action: "view",
+          label: "Køb for helvede!",
+          url: config.eventUrl,
+        },
+      ],
+    }),
+  }).catch((error) => {
+    console.error("Error sending notification:", error);
+  });
+};
+
+const sendErrorNotification = (error) => {
+  console.log("Sending error notification...");
+
+  fetch("https://ntfy.israndom.win", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      topic: ntfyTopic,
+      title: "Error checking tickets",
+      message: `An error occurred while checking tickets for "${config.eventName}".\n\n${error}`,
+    }),
+  }).catch((error) => {
+    console.error("Error sending error notification:", error);
+  });
+};
+
+checkForTickets()
+  .then((data) => {
+    if (data.offers.length > 0) {
+      console.log(JSON.stringify(data));
+      console.log();
+
+      console.log("TICKETS AVAILABLE?!" + "\n");
+      sendSuccessNotification();
+    } else {
+      console.log("NO TICKETS AVAILABLE");
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+    sendErrorNotification(error);
+  });
